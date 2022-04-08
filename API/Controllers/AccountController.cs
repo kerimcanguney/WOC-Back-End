60c48114fdebfa968b5b3f7d8b8cb1f4078f59ef
Kerimcan <464639@student.fontys.nl>
2022-04-08 09:20:36 +0200

    Generate and validate tokens in AccountController.cs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,9 +2,14 @@
 using API.Models;
 using API.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -14,9 +19,12 @@ namespace API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAccountService _service;
-        public AccountController(IAccountContext context)
+        private IConfiguration _config;
+
+        public AccountController(IAccountContext context, IConfiguration config)
         {
             _service = new AccountService(context);
+            _config = config; ;
         }
         [HttpGet]
         public IEnumerable<Account> Get()
@@ -25,10 +33,54 @@ namespace API.Controllers
             //_service.RegisterAccount(acc);
             return _service.GetAccounts();
         }
-        [HttpPost]
-        public Account Login(string email, string password)
+
+        [HttpPost("Login")]
+        public string Login(string email, string password)
         {
-            return _service.LoginAccount(email,password);
+            Account loginAccount = _service.LoginAccount(email, password);
+
+            return Generate(loginAccount);
+        }
+        private string Generate(Account account)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, account.Name),
+                new Claim(ClaimTypes.Email, account.Email),
+                //new Claim(ClaimTypes.Role, account.Role.Name) 
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        private Account GetCurrentUserViaHttpContext()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity == null) return null;
+
+            var userClaims = identity.Claims;
+
+            return new Account
+            {
+                Name = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+                Email = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                Role = new Role() { Name = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value }
+
+            };
+        }
+        [HttpPost("Register")]
+        public string Register(string username, string email, string password)
+        {
+            return Generate(new Account() { Name = username, Email = email, Password = password });
         }
     }
 }
